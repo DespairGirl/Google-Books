d76bdea887d17c1b86922ec6b376ac6963a70512
Ult Des <truttling.mariah@gmail.com>
2021-07-29 13:34:17 -0400

    finished book controller

---
+++ create-react-express/models/book.js
@@ -0,0 +1,16 @@
+const mongoose= require("mongoose");
+const Schema=mongoose.Schema;
+
+const bookSchema=new Schema({
+title: {type:String},
+authors: {type:String},
+description: {type:String},
+image: {type:String},
+link: {type:String}
+
+
+});
+
+const Books=mongoose.model("Book", bookSchema);
+
+module.exports=Books;
